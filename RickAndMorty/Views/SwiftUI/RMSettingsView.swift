1bef0570d75aa174c797201cb622b8719f9c980e
javenkn <javenkn7@gmail.com>
2023-02-11 18:18:52 -1000

    Creates the settings screen UI in SwiftUI

---
+++ RickAndMorty/Views/SwiftUI/RMSettingsView.swift
@@ -0,0 +1,44 @@
+//
+//  RMSettingsView.swift
+//  RickAndMorty
+//
+//  Created by J on 2/11/23.
+//
+
+import SwiftUI
+
+struct RMSettingsView: View {
+    let viewModel: RMSettingsViewViewModel
+    
+    init(viewModel: RMSettingsViewViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    var body: some View {
+        List(viewModel.cellViewModels) { viewModel in
+            HStack {
+                if let image = viewModel.image {
+                    Image(uiImage: image)
+                        .resizable()
+                        .renderingMode(.template)
+                        .foregroundColor(.white)
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 20, height: 20)
+                        .padding(10)
+                        .background(Color(viewModel.iconContainerColor))
+                        .cornerRadius(8)
+                }
+                Text(viewModel.title)
+                    .padding(.leading, 10)
+            }
+        }
+    }
+}
+
+struct RMSettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        RMSettingsView(viewModel: .init(cellViewModels: RMSettingsOption.allCases.compactMap({
+            return RMSettingsCellViewModel(type: $0)
+        })))
+    }
+}
