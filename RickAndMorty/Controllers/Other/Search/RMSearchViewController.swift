f625411bf7509de622b5cc4c24d5bc67819a8d71
javenkn <javenkn7@gmail.com>
2023-02-18 20:54:37 -1000

    Implements search request execution logic

--- RickAndMorty/Controllers/Other/Search/RMSearchViewController.swift
+++ RickAndMorty/Controllers/Other/Search/RMSearchViewController.swift
@@ -16,6 +16,17 @@ class RMSearchViewController: UIViewController {
             case episode // name
             case location // name | planet
             
+            var endpoint: RMEndpoint {
+                switch self {
+                case .character:
+                    return .character
+                case .episode:
+                    return .episode
+                case .location:
+                    return .location
+                }
+            }
+            
             var title: String {
                 switch self {
                 case .character:
@@ -72,7 +83,7 @@ class RMSearchViewController: UIViewController {
     
     @objc
     private func didTapExecuteSearch() {
-//        viewModel.executeSeach()
+        viewModel.executeSearch()
     }
     
     private func addConstraints() {
--- RickAndMorty/ViewModels/Search/RMSearchViewViewModel.swift
+++ RickAndMorty/ViewModels/Search/RMSearchViewViewModel.swift
@@ -10,6 +10,7 @@ import Foundation
 final class RMSearchViewViewModel {
     let config: RMSearchViewController.Config
     private var optionMapUpdateBlock: (((RMSearchInputViewViewModel.DynamicOption, String)) -> Void)?
+    private var searchResultHandler: (() -> Void)?
     
     private var optionMap: [RMSearchInputViewViewModel.DynamicOption: String] = [:]
     private var searchText = ""
@@ -22,8 +23,42 @@ final class RMSearchViewViewModel {
     
     // MARK: - Public
     
+    public func registerSearchResultHandler(_ block: @escaping () -> Void) {
+        self.searchResultHandler = block
+    }
+    
     public func executeSearch() {
+        searchText = "Rick"
         
+        // Build arguments
+        var queryParams: [URLQueryItem] = [
+            URLQueryItem(name: "name", value: searchText)
+        ]
+        
+        // Add options
+        queryParams.append(contentsOf: optionMap.enumerated().compactMap({ _, element in
+            let key: RMSearchInputViewViewModel.DynamicOption = element.key
+            let value: String = element.value
+            return URLQueryItem(name: key.queryArgument, value: value)
+        }))
+        
+        // Create request
+        let request = RMRequest(
+            endpoint: config.filterType.endpoint,
+            queryParams: queryParams
+        )
+        
+        print(request.url?.absoluteURL)
+        
+        // Execute request
+        RMService.shared.execute(request, expecting: RMGetAllCharactersResponse.self) { result in
+            switch result {
+            case .success(let model):
+                print(String(describing: model.results))
+            case .failure:
+                break
+            }
+        }
     }
     
     public func set(query text: String) {
