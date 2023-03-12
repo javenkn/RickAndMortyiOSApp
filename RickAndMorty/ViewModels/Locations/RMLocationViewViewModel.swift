63ac7ae537b867a826b8e776591aedaa989597b2
javenkn <javenkn7@gmail.com>
2023-03-12 13:45:47 -1000

    Implements pagination for the locations tab

--- RickAndMorty/ViewModels/Locations/RMLocationViewViewModel.swift
+++ RickAndMorty/ViewModels/Locations/RMLocationViewViewModel.swift
@@ -26,7 +26,68 @@ final class RMLocationViewViewModel {
     private var apiInfo: RMGetAllLocationsResponse.Info?
     public private(set) var cellViewModels: [RMLocationTableViewCellViewModel] = []
     
+    public var shouldShowLoadMoreIndicator: Bool {
+        return apiInfo?.next != nil
+    }
+    
+    public var isLoadingMoreLocations = false
+    
+    // MARK: - Init
+    
     init() {}
+    
+    /// Paginate if additional locations are needed
+    public func fetchAdditionalLocations() {
+        guard !isLoadingMoreLocations else {
+            return
+        }
+        
+        guard let nextUrlString = apiInfo?.next,
+              let url = URL(string: nextUrlString) else {
+            return
+        }
+        
+        isLoadingMoreLocations = true
+        
+        guard let request = RMRequest(url: url) else {
+            isLoadingMoreLocations = false
+            print("Failed to create request")
+            return
+        }
+        
+        RMService.shared.execute(
+            request,
+            expecting: RMGetAllLocationsResponse.self) { [weak self] result in
+                guard let strongSelf = self else {
+                    return
+                }
+                switch result {
+                case .success(let responseModel):
+                    let moreResults = responseModel.results
+                    let info = responseModel.info
+                    print("More locations: \(moreResults.count)")
+//                    let originalCount = strongSelf.episodes.count
+//                    let newCount = moreResults.count
+//                    let total = originalCount + newCount
+//                    let startingIndex = total - newCount
+//                    let indexPathsToAdd: [IndexPath] = Array(startingIndex..<(startingIndex+newCount)).compactMap({
+//                        return IndexPath(row: $0, section: 0)
+//                    })
+//
+//                    strongSelf.episodes.append(contentsOf: moreResults)
+                    strongSelf.apiInfo = info
+                    strongSelf.cellViewModels.append(contentsOf: moreResults.compactMap({
+                        return RMLocationTableViewCellViewModel(location: $0)
+                    }))
+                    DispatchQueue.main.async {
+                        strongSelf.isLoadingMoreLocations = false
+                    }
+                case .failure(let failure):
+                    print(String(describing: failure))
+                    strongSelf.isLoadingMoreLocations = false
+                }
+            }
+    }
     
     public func location(at index: Int) -> RMLocation? {
         guard index < locations.count, index >= 0 else {
---
+++ RickAndMorty/Views/Location/RMTableLoadingFooterView.swift
@@ -0,0 +1,39 @@
+//
+//  RMTableLoadingFooterView.swift
+//  RickAndMorty
+//
+//  Created by J on 3/12/23.
+//
+
+import UIKit
+
+final class RMTableLoadingFooterView: UIView {
+    private let spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView()
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.isHidden = true
+        return spinner
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(spinner)
+        spinner.startAnimating()
+        
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    private func addConstraints() {
+        NSLayoutConstraint.activate([
+            spinner.widthAnchor.constraint(equalToConstant: 55),
+            spinner.heightAnchor.constraint(equalToConstant: 55),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor),
+        ])
+    }
+}
